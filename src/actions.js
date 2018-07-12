acbb11c3fb22a5e988c26b5fa0860fd982ccded0
Cynnah <massot.dorian@gmail.com>
2018-07-12 11:47:32 +0200

    create action delete booking id

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,12 @@
 export const BOOKINGS_RECEIVED = "BOOKINGS_RECEIVED";
+export const DELETE_BOOKING = "DELETE_BOOKING";
 
 export const makeBookingsReceived = bookings => ({
   type: BOOKINGS_RECEIVED,
   bookings
+});
+
+export const makeDeletedBookingId = id => ({
+  type: DELETE_BOOKING,
+  id
 });
--- src/reducers/bookings.js
+++ src/reducers/bookings.js
@@ -6,6 +6,10 @@ export default (prevState = initialState, action) => {
   if (action.type === BOOKINGS_RECEIVED) {
     return action.bookings;
   }
+  // creer l'action la test dans redux dev tools a la main
+  // et une fois que ça marche
+  //gerer l'action delete booking
+  // filter booking qui ont un id diff de bookingId
 
   return prevState;
 };
